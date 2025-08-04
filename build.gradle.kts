fbf4895b9e4f70aeede3e68e0eb057aac4405fb0
minguni <80194344+min-guni@users.noreply.github.com>
2025-08-05 01:05:31 +0900

    fix : 라이브러리용으로 변경하기

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,36 @@
 plugins {
+  kotlin("jvm") version "1.9.24"
+  kotlin("plugin.spring") version "1.9.24"
   `java-library`
   `maven-publish`
 }
 
 group = "vottega"
-version = "1.0.0"
+version = "1.0.3"
 
 java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
+
+dependencies {
+  api(platform("org.springframework.boot:spring-boot-dependencies:3.3.2"))
+
+  api("org.springframework.security:spring-security-core")
+
+  compileOnly("org.springframework.security:spring-security-web")
+  compileOnly("org.springframework.security:spring-security-config")
+
+  compileOnly("org.springframework:spring-web")
+  compileOnly("jakarta.servlet:jakarta.servlet-api")
+  compileOnly("org.springframework:spring-webflux")
+  compileOnly("io.projectreactor:reactor-core")
+
+
+  compileOnly("org.springframework.boot:spring-boot-autoconfigure")
+  compileOnly("org.springframework.boot:spring-boot")
+}
+
+repositories {
+  mavenCentral()
+}
 
 publishing {
   publications {

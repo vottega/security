93d8aee9214f76172244703553e4fce017973897
minguni <80194344+min-guni@users.noreply.github.com>
2025-08-06 02:17:40 +0900

    fix : pattern matching 오류 수정

--- src/main/kotlin/vottega/security/mvc/CustomHeaderAuthenticationFilter.kt
+++ src/main/kotlin/vottega/security/mvc/CustomHeaderAuthenticationFilter.kt
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import org.springframework.util.AntPathMatcher
 import org.springframework.web.filter.OncePerRequestFilter
 import vottega.security.security.ClientRole
 import vottega.security.security.CustomParticipantRoleAuthenticationToken
@@ -18,9 +19,11 @@ class CustomHeaderAuthenticationFilter(
   private val props: SecurityProps
 ) : OncePerRequestFilter() {
 
+
+  private val matcher = AntPathMatcher()
+
   override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
     val headerRole = request.getHeader(props.headerNames.role)
-    println(request.getHeader(props.headerNames.userId))
     val userId = request.getHeader(props.headerNames.userId)?.toLongOrNull()
     val participantUUID = try {
       UUID.fromString(request.getHeader(props.headerNames.participantId))
@@ -60,5 +63,6 @@ class CustomHeaderAuthenticationFilter(
   }
 
   override fun shouldNotFilter(request: HttpServletRequest) =
-    !props.filterPaths.any { request.servletPath.startsWith(it) }
+    props.filterPaths
+      .none { pattern -> matcher.match(pattern, request.servletPath) }
 }
